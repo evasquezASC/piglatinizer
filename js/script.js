6ab9c66e950b2728c738830526230d1793773801
evasquezASC <ernievazk7979@gmail.com>
2019-10-29 22:13:39 +0000

    I added an array, click function, and an if-else statement

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+$("#button").click(function() {
+    let message = $(".input").val();
+    if ( message === vowels) {
+        
+}
+});
 
 
-  
-  
-  
-  
+
+
 });
